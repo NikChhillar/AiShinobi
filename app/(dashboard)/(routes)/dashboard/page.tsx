026436eadb9f29d1143ebbb976a6a09cbb2d4d20
Nikhil <nikhilchhillar29@gmail.com>
2023-07-21 09:56:31 +0530

    added sidebar

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+const DashboardPage = () => {
+  const router = useRouter();
+
+  return <div>DashboardPage</div>;
+};
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      LandingPage
+      <Link href={"/sign-in"}>
+        <Button>Login</Button>
+      </Link>
+      <Link href={"/sign-up"}>
+        <Button>Sign up</Button>
+      </Link>
+    </div>
+  );
+};
+
+export default LandingPage;

80f83cfebf11808cbabcfd3963f5972b00c97567
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-08-16 21:30:16 +0530

    Improved 🤍

--- app/(dashboard)/(routes)/creator/page.tsx
+++ app/(dashboard)/(routes)/creator/page.tsx
@@ -1,4 +1,5 @@
 import { Heading } from '@/components/Heading'
+import { Card, CardContent, CardFooter } from '@/components/ui/card'
 import { Separator } from '@/components/ui/separator'
 import { StarIcon } from 'lucide-react'
 import React from 'react'
@@ -14,21 +15,20 @@ const CreatorPage = () => {
                 bgColor="bg-yellow-500/10"
             />
             <div className="p-8">
-                <h2 className="text-2xl font-semibold mb-4">Top 5 Animes</h2>
+                <h2 className="text-2xl font-semibold mb-4">Favorite Animes</h2>
                 {/* Display your top 5 favorite animes */}
             </div>
             <Separator className='mt-4' />
             <div className="p-8">
-                <h2 className="text-2xl font-semibold mb-4">Top 5 characters</h2>
+                <h2 className="text-2xl font-semibold mb-4">Favorite characters</h2>
                 {/* Display your top 5 favorite characters */}
             </div>
             <Separator className='mt-4' />
 
             <div className="p-8">
-                <h2 className="text-2xl font-semibold mb-4">Favourite quotes</h2>
+                <h2 className="text-2xl font-semibold mb-4">Favorite quotes</h2>
                 {/* Display your top 5 favorite quotes */}
             </div>
-
         </div>
     )
 }

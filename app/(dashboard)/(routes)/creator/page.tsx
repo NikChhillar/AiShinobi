dc5d0af5373e707bf7de6b26ffef1ea0766d8e29
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-08-16 21:10:49 +0530

    added separator 🔗

--- app/(dashboard)/(routes)/creator/page.tsx
+++ app/(dashboard)/(routes)/creator/page.tsx
@@ -1,4 +1,5 @@
 import { Heading } from '@/components/Heading'
+import { Separator } from '@/components/ui/separator'
 import { StarIcon } from 'lucide-react'
 import React from 'react'
 
@@ -12,6 +13,21 @@ const CreatorPage = () => {
                 iconColor="text-yellow-500"
                 bgColor="bg-yellow-500/10"
             />
+            <div className="p-8">
+                <h2 className="text-2xl font-semibold mb-4">Top 5 Animes</h2>
+                {/* Display your top 5 favorite animes */}
+            </div>
+            <Separator className='mt-4' />
+            <div className="p-8">
+                <h2 className="text-2xl font-semibold mb-4">Top 5 characters</h2>
+                {/* Display your top 5 favorite characters */}
+            </div>
+            <Separator className='mt-4' />
+
+            <div className="p-8">
+                <h2 className="text-2xl font-semibold mb-4">Favourite quotes</h2>
+                {/* Display your top 5 favorite quotes */}
+            </div>
 
         </div>
     )

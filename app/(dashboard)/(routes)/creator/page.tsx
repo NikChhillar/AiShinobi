946f49129e63480132a0ace5e51eaf905a08f767
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-08-17 08:22:41 +0530

    improved 🤍

--- app/(dashboard)/(routes)/creator/page.tsx
+++ app/(dashboard)/(routes)/creator/page.tsx
@@ -19,26 +19,26 @@ const CreatorPage = () => {
             <div className="p-8">
                 <h2 className="text-2xl font-semibold mb-4">Favorite Animes</h2>
                 {/* Display your top 5 favorite animes */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
 
-                    <AnimeCard rank={1} animeName='That time I got reincarnated as a slime' imageUrl='/logo.png' />
-                    <AnimeCard rank={2} animeName='Fruit Basket' imageUrl='/logo.png' />
-                    <AnimeCard rank={3} animeName='Bocchi the Rock' imageUrl='/logo.png' />
-                    <AnimeCard rank={4} animeName='Love is War' imageUrl='/logo.png' />
-                    <AnimeCard rank={5} animeName='Fullmetal Alchemist Brotherhood' imageUrl='/logo.png' />
+                    <AnimeCard rank={1} animeName='That time I got reincarnated as a slime' imageUrl='/images/tensura.jpg' />
+                    <AnimeCard rank={2} animeName='Fruit Basket' imageUrl='/images/fb.jpg' />
+                    <AnimeCard rank={3} animeName='Bocchi the Rock' imageUrl='/images/lw.jpg' />
+                    <AnimeCard rank={4} animeName='Love is War' imageUrl='/images/lw.jpg' />
+                    <AnimeCard rank={5} animeName='Fullmetal Alchemist Brotherhood' imageUrl='/images/lw.jpg' />
                 </div>
             </div>
             <Separator className='mt-4' />
             <div className="p-8">
                 <h2 className="text-2xl font-semibold mb-4">Favorite characters</h2>
                 {/* Display your top 5 favorite characters */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
 
-                    <CharacterCard characterName='X' rank={1} animeName='One piece' imageUrl='/logo.png' />
-                    <CharacterCard characterName='X' rank={2} animeName='One piece' imageUrl='/logo.png' />
-                    <CharacterCard characterName='X' rank={3} animeName='One piece' imageUrl='/logo.png' />
-                    <CharacterCard characterName='X' rank={4} animeName='One piece' imageUrl='/logo.png' />
-                    <CharacterCard characterName='X' rank={5} animeName='One piece' imageUrl='/logo.png' />
+                    <CharacterCard characterName='Miku' rank={1} animeName='One piece' imageUrl='/images/miku.jpg' />
+                    <CharacterCard characterName='Kirito' rank={2} animeName='One piece' imageUrl='/images/kirito.jpg' />
+                    <CharacterCard characterName='Anya' rank={3} animeName='One piece' imageUrl='/images/anya.jpg' />
+                    <CharacterCard characterName='X' rank={4} animeName='One piece' imageUrl='/images/lw.jpg' />
+                    <CharacterCard characterName='X' rank={5} animeName='One piece' imageUrl='/images/lw.jpg' />
                 </div>
             </div>
             <Separator className='mt-4' />

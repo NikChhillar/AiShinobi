614e78d40e14260b0f16d71c3d96520ec2326337
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-08-18 11:49:54 +0530

    improved

--- app/(dashboard)/(routes)/about/page.tsx
+++ app/(dashboard)/(routes)/about/page.tsx
@@ -13,19 +13,24 @@ const page = () => {
         bgColor="bg-violet-500/10"
       />
 
-      <div className="flex-1 px-4 md:px-32">
-        <p className="text-sm mb-4">
-          AniHub is your ultimate destination to celebrate your passion for anime. With a mission to empower anime enthusiasts, AniHub offers a platform to curate and organize your anime universe effortlessly.
-        </p>
-        <p className="text-sm mb-4">
-          From crafting your top 15 anime collection to showcasing iconic characters and preserving unforgettable quotes, AniHub brings together the essential elements that define your anime experience.
-        </p>
-        <p className="text-sm mb-4">
-          Seamlessly manage your watchlist, watch later list, and completed list with intuitive drag-and-drop functionality. Dive into the world of anime with AniHub and make your mark on the anime realm.
-        </p>
-        <p className="text-sm">
-          Join us in building a community that celebrates the art, storytelling, and emotions of anime. AniHub is where your anime journey begins!
-        </p>
+      <div className="container mx-auto mt-8 px-4 md:px-16 lg:px-32">
+        <div className="rounded-lg shadow-lg bg-white p-8">
+          <p className="text-gray-700 text-lg mb-4">
+            Welcome to <span className="font-bold text-purple-500">AniHub</span> – Your Ultimate Anime Experience!
+          </p>
+          <p className="text-gray-600 mb-4">
+            At AniHub, we&apos;re dedicated to bringing your love for anime to a whole new level. Our platform is designed to be your personal haven for all things anime. Whether you&apos;re an avid otaku or a casual watcher, AniHub is here to empower you to celebrate and cherish your favorite anime series, characters, and quotes.
+          </p>
+          <p className="text-gray-600 mb-4">
+            With AniHub, you have the power to curate and organize your anime universe effortlessly. From creating your top 15 anime collection to showcasing iconic characters and preserving unforgettable quotes, we&apos;ve got it all covered. Seamlessly manage your watchlist, watch later list, and completed list with intuitive drag-and-drop functionality.
+          </p>
+          <p className="text-gray-600 mb-4">
+            Our community is a place where anime lovers can come together to share their passion, exchange recommendations, and build connections. We believe that anime is not just a form of entertainment, but a journey that brings people closer and fosters creativity and imagination.
+          </p>
+          <p className="text-gray-600">
+            Join us on this exciting adventure, where your anime journey is enriched, celebrated, and shared. Let&apos;s explore the vast anime universe together with AniHub!
+          </p>
+        </div>
       </div>
     </div>
   )
